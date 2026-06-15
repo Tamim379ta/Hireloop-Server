28c48415e5f0d8cfaa6fff1fb5e71e7e8e67df93
Tamim379ta <tamimthasan55@gmail.com>
2026-06-15 21:20:50 +0600

    added jobs pagination

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const database = client.db("hireloop");
     const jobsCollection = database.collection("jobs");
     const companyCollection = database.collection("companies");
@@ -269,8 +269,8 @@ async function run() {
     })
 
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // await client.close();
   }
